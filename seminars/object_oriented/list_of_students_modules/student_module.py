de10b1d48d78af90ca1b20a4a44947fd69984a3f
Administrator <admin@example.com>
2020-01-31 14:25:54 +0200

    lesson.23.1.2020

---
+++ seminars/object_oriented/list_of_students_modules/student_module.py
@@ -0,0 +1,8 @@
+# OOP Assignment:
+# Date: Jan 2020
+
+
+# Develop a separated module the includes the definition for the Student class.
+# The variables each Student object should have are 'firstName', 'lastName', 'id' and 'average'.
+class Student:
+    self.firstName =
